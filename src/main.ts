4d4f9c8644a655f41d7ad9dda872dd19876f6d03
testcs50 <assayf95@gmail.com>
2024-06-15 19:36:00 +0300

    initial

---
+++ src/main.ts
@@ -0,0 +1,72 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import GUI from "lil-gui";
+import "./style.css";
+
+const gui = new GUI({ width: 340 });
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+const scene = new THREE.Scene();
+
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  new THREE.MeshBasicMaterial()
+);
+scene.add(cube);
+
+gui.add(cube.position, "x").min(-3).max(3).step(0.01).name("Cube X");
+gui.add(cube.position, "y").min(-3).max(3).step(0.01).name("Cube Y");
+gui.add(cube.position, "z").min(-3).max(3).step(0.01).name("Cube Z");
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 1, 1);
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  cube.rotation.y = elapsedTime;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
