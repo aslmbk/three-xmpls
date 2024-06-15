0ed9b422ee208147076c37fccd9c7d178b4c5d42
testcs50 <assayf95@gmail.com>
2024-06-16 02:51:55 +0300

    sea

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { Timer } from "three/addons/misc/Timer.js";
 import GUI from "lil-gui";
+import VS from "./shaders/vertex.glsl";
+import FS from "./shaders/fragment.glsl";
 import "./style.css";
 
 const gui = new GUI({ width: 340 });
+const debugObject = {
+  depthColor: "#186691",
+  surfaceColor: "#9bd8ff",
+};
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
@@ -11,15 +18,96 @@ const sizes = {
 const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
 const scene = new THREE.Scene();
 
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(),
-  new THREE.MeshBasicMaterial()
-);
-scene.add(cube);
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 1024, 1024);
+const planeMaterial = new THREE.ShaderMaterial({
+  vertexShader: VS,
+  fragmentShader: FS,
+  uniforms: {
+    uTime: { value: 0 },
+    uBigWavesElevation: { value: 0.2 },
+    uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+    uBigWavesSpeed: { value: 0.75 },
+    uSmallWavesElevation: { value: 0.15 },
+    uSmallWavesFrequency: { value: 3 },
+    uSmallWavesSpeed: { value: 0.2 },
+    uSmallIterations: { value: 4 },
+    uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
+    uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
+    uColorOffset: { value: 0.08 },
+    uColorMultiplier: { value: 5 },
+  },
+});
 
-gui.add(cube.position, "x").min(-3).max(3).step(0.01).name("Cube X");
-gui.add(cube.position, "y").min(-3).max(3).step(0.01).name("Cube Y");
-gui.add(cube.position, "z").min(-3).max(3).step(0.01).name("Cube Z");
+gui.addColor(debugObject, "depthColor").onChange(() => {
+  planeMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor);
+});
+gui.addColor(debugObject, "surfaceColor").onChange(() => {
+  planeMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
+});
+gui
+  .add(planeMaterial.uniforms.uBigWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uBigWavesElevation");
+gui
+  .add(planeMaterial.uniforms.uBigWavesFrequency.value, "x")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uBigWavesFrequencyX");
+gui
+  .add(planeMaterial.uniforms.uBigWavesFrequency.value, "y")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uBigWavesFrequencyY");
+gui
+  .add(planeMaterial.uniforms.uBigWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uBigWavesSpeed");
+gui
+  .add(planeMaterial.uniforms.uSmallWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uSmallWavesElevation");
+gui
+  .add(planeMaterial.uniforms.uSmallWavesFrequency, "value")
+  .min(0)
+  .max(30)
+  .step(0.001)
+  .name("uSmallWavesFrequency");
+gui
+  .add(planeMaterial.uniforms.uSmallWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uSmallWavesSpeed");
+gui
+  .add(planeMaterial.uniforms.uSmallIterations, "value")
+  .min(0)
+  .max(5)
+  .step(1)
+  .name("uSmallIterations");
+gui
+  .add(planeMaterial.uniforms.uColorOffset, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uColorOffset");
+gui
+  .add(planeMaterial.uniforms.uColorMultiplier, "value")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uColorMultiplier");
+
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -Math.PI * 0.5;
+scene.add(plane);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -38,34 +126,28 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 window.addEventListener("resize", () => {
-  // Update sizes
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
-  // Update camera
   camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
 
-  // Update renderer
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-const clock = new THREE.Clock();
+const timer = new Timer();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+  timer.update();
+  const elapsedTime = timer.getElapsed();
 
-  // Update objects
-  cube.rotation.y = elapsedTime;
+  planeMaterial.uniforms.uTime.value = elapsedTime;
 
-  // Update controls
   controls.update();
 
-  // Render
   renderer.render(scene, camera);
 
-  // Call tick again on the next frame
   window.requestAnimationFrame(tick);
 };
 
