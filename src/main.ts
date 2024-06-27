fec4a50669a76783e6115f035fddd92150895fd8
testcs50 <assayf95@gmail.com>
2024-06-27 16:07:39 +0300

    cursor animation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { Timer } from "three/addons/misc/Timer.js";
 import GUI from "lil-gui";
 import vertexShader from "./shaders/vertex.glsl";
@@ -7,94 +9,40 @@ import fragmentShader from "./shaders/fragment.glsl";
 import "./style.css";
 
 const gui = new GUI({ width: 340 });
-const parameters = {};
+const parameters = {
+  clearColor: "#160920",
+};
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
   pixelRatio: Math.min(window.devicePixelRatio, 2),
 };
 const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
-const textureLoader = new THREE.TextureLoader();
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("./draco/");
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
 const scene = new THREE.Scene();
 
-const displacement = {
-  canvas: document.createElement("canvas"),
-  context: {} as CanvasRenderingContext2D,
-  texture: {} as THREE.Texture,
-  glowImage: new Image(),
-  interactivePlane: new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshBasicMaterial({ color: "red", side: THREE.DoubleSide })
-  ),
-  raycaster: new THREE.Raycaster(),
-  screenCursor: new THREE.Vector2(9999, 9999),
-  canvasCursor: new THREE.Vector2(9999, 9999),
-  canvasCursorPrevious: new THREE.Vector2(9999, 9999),
+const particles = {
+  geometry: new THREE.SphereGeometry(3),
+  material: new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms: {
+      uSize: new THREE.Uniform(0.4),
+      uResolution: new THREE.Uniform(
+        new THREE.Vector2(
+          sizes.width * sizes.pixelRatio,
+          sizes.height * sizes.pixelRatio
+        )
+      ),
+    },
+  }),
+  points: {} as THREE.Points,
 };
-
-displacement.context = displacement.canvas.getContext("2d")!;
-displacement.texture = new THREE.CanvasTexture(displacement.canvas);
-displacement.glowImage.src = "./glow.png";
-displacement.canvas.width = 128;
-displacement.canvas.height = 128;
-displacement.canvas.style.position = "fixed";
-displacement.canvas.style.width = "256px";
-displacement.canvas.style.height = "256px";
-displacement.canvas.style.top = "0";
-displacement.canvas.style.left = "0";
-displacement.canvas.style.zIndex = "10";
-document.body.append(displacement.canvas);
-
-displacement.context.fillRect(
-  0,
-  0,
-  displacement.canvas.width,
-  displacement.canvas.height
-);
-
-scene.add(displacement.interactivePlane);
-displacement.interactivePlane.visible = false;
-
-const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128);
-particlesGeometry.setIndex(null);
-particlesGeometry.deleteAttribute("normal");
-
-const intensitiesArray = new Float32Array(
-  particlesGeometry.attributes.position.count
-);
-const anglesArray = new Float32Array(
-  particlesGeometry.attributes.position.count
-);
-for (let i = 0; i < intensitiesArray.length; i++) {
-  intensitiesArray[i] = Math.random();
-  anglesArray[i] = Math.random() * Math.PI * 2;
-}
-particlesGeometry.setAttribute(
-  "aIntensity",
-  new THREE.BufferAttribute(intensitiesArray, 1)
-);
-particlesGeometry.setAttribute(
-  "aAngle",
-  new THREE.BufferAttribute(anglesArray, 1)
-);
-
-const particlesMaterial = new THREE.ShaderMaterial({
-  vertexShader,
-  fragmentShader,
-  uniforms: {
-    uResolution: new THREE.Uniform(
-      new THREE.Vector2(
-        sizes.width * sizes.pixelRatio,
-        sizes.height * sizes.pixelRatio
-      )
-    ),
-    uPictureTexture: new THREE.Uniform(textureLoader.load("./picture-2.png")),
-    uDisplacementTexture: new THREE.Uniform(displacement.texture),
-  },
-  transparent: true,
-});
-const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(particles);
+particles.points = new THREE.Points(particles.geometry, particles.material);
+scene.add(particles.points);
 
 const camera = new THREE.PerspectiveCamera(
   35,
@@ -102,20 +50,19 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(0, 0, 18);
+camera.position.set(0, 0, 16);
 scene.add(camera);
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(sizes.pixelRatio);
-renderer.setClearColor("#181818");
+renderer.setClearColor(parameters.clearColor);
 
-window.addEventListener("pointermove", (event) => {
-  displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1;
-  displacement.screenCursor.y = -(event.clientY / sizes.height) * 2 + 1;
+gui.addColor(parameters, "clearColor").onChange(() => {
+  renderer.setClearColor(parameters.clearColor);
 });
 
 window.addEventListener("resize", () => {
@@ -123,7 +70,7 @@ window.addEventListener("resize", () => {
   sizes.height = window.innerHeight;
   sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
-  particlesMaterial.uniforms.uResolution.value.set(
+  particles.material.uniforms.uResolution.value.set(
     sizes.width * sizes.pixelRatio,
     sizes.height * sizes.pixelRatio
   );
@@ -140,45 +87,6 @@ const timer = new Timer();
 const tick = () => {
   timer.update();
   const elapsedTime = timer.getElapsed();
-
-  displacement.raycaster.setFromCamera(displacement.screenCursor, camera);
-  const intersects = displacement.raycaster.intersectObject(
-    displacement.interactivePlane
-  );
-
-  if (intersects.length) {
-    const { x, y } = intersects[0].uv!;
-    displacement.canvasCursor.x = x * displacement.canvas.width;
-    displacement.canvasCursor.y = (1 - y) * displacement.canvas.height;
-  }
-
-  displacement.context.globalCompositeOperation = "source-over";
-  displacement.context.globalAlpha = 0.02;
-  displacement.context.fillRect(
-    0,
-    0,
-    displacement.canvas.width,
-    displacement.canvas.height
-  );
-
-  const cursorDistance = displacement.canvasCursorPrevious.distanceTo(
-    displacement.canvasCursor
-  );
-  displacement.canvasCursorPrevious.copy(displacement.canvasCursor);
-  const alpha = Math.min(cursorDistance * 0.05, 1);
-
-  const glowSize = displacement.canvas.width * 0.25;
-  displacement.context.globalCompositeOperation = "lighten";
-  displacement.context.globalAlpha = alpha;
-  displacement.context.drawImage(
-    displacement.glowImage,
-    displacement.canvasCursor.x - glowSize * 0.5,
-    displacement.canvasCursor.y - glowSize * 0.5,
-    glowSize,
-    glowSize
-  );
-
-  displacement.texture.needsUpdate = true;
 
   controls.update();
 
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,38 +1,13 @@
 uniform vec2 uResolution;
-uniform sampler2D uPictureTexture;
-uniform sampler2D uDisplacementTexture;
-
-attribute float aIntensity;
-attribute float aAngle;
-
-varying float vPictureIntensity;
+uniform float uSize;
 
 void main()
 {
-    vec3 newPosition = position;
-    float displacementIntensity = texture(uDisplacementTexture, uv).r;
-    displacementIntensity = smoothstep(0.1, 0.3, displacementIntensity);
-
-    vec3 displacement = vec3(
-        cos(aAngle) * 0.2,
-        sin(aAngle) * 0.2,
-        1.0
-    );
-    displacement = normalize(displacement);
-    displacement *= aIntensity * displacementIntensity * 3.0;
-
-    newPosition += displacement;
-
-    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
-
-    float pictureIntensity = texture(uPictureTexture, uv).r;
     
-    gl_PointSize = 0.14 * uResolution.y;
+    gl_PointSize = uSize * uResolution.y;
     gl_PointSize *= (1.0 / - viewPosition.z);
-    gl_PointSize *= pictureIntensity;
-
-    vPictureIntensity = pow(pictureIntensity, 2.0);
 }
