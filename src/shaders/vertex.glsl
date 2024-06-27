bce19647ea29435544051b54ad18da94ead7c2c1
testcs50 <assayf95@gmail.com>
2024-06-27 16:24:21 +0300

    Revert "cursor animation"
    This reverts commit fec4a50669a76783e6115f035fddd92150895fd8.

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,13 +1,38 @@
 uniform vec2 uResolution;
-uniform float uSize;
+uniform sampler2D uPictureTexture;
+uniform sampler2D uDisplacementTexture;
+
+attribute float aIntensity;
+attribute float aAngle;
+
+varying float vPictureIntensity;
 
 void main()
 {
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec3 newPosition = position;
+    float displacementIntensity = texture(uDisplacementTexture, uv).r;
+    displacementIntensity = smoothstep(0.1, 0.3, displacementIntensity);
+
+    vec3 displacement = vec3(
+        cos(aAngle) * 0.2,
+        sin(aAngle) * 0.2,
+        1.0
+    );
+    displacement = normalize(displacement);
+    displacement *= aIntensity * displacementIntensity * 3.0;
+
+    newPosition += displacement;
+
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
+
+    float pictureIntensity = texture(uPictureTexture, uv).r;
     
-    gl_PointSize = uSize * uResolution.y;
+    gl_PointSize = 0.14 * uResolution.y;
     gl_PointSize *= (1.0 / - viewPosition.z);
+    gl_PointSize *= pictureIntensity;
+
+    vPictureIntensity = pow(pictureIntensity, 2.0);
 }
