91539b8b90f901c06be7f97af90f349c64671424
testcs50 <assayf95@gmail.com>
2024-06-30 06:26:39 +0300

    morphing

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { Timer } from "three/addons/misc/Timer.js";
+import gsap from "gsap";
 import GUI from "lil-gui";
 import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
@@ -25,7 +26,7 @@ gltfLoader.setDRACOLoader(dracoLoader);
 const scene = new THREE.Scene();
 
 const particles = {
-  geometry: new THREE.SphereGeometry(3),
+  geometry: new THREE.BufferGeometry(),
   material: new THREE.ShaderMaterial({
     vertexShader,
     fragmentShader,
@@ -37,12 +38,99 @@ const particles = {
           sizes.height * sizes.pixelRatio
         )
       ),
+      uProgress: new THREE.Uniform(0),
+      uColorA: new THREE.Uniform(new THREE.Color("#ff7300")),
+      uColorB: new THREE.Uniform(new THREE.Color("#0091ff")),
     },
+    transparent: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
   }),
   points: {} as THREE.Points,
+  modelPositions: {} as Record<string, THREE.BufferAttribute>,
+  maxPositionCount: 0,
+  morphingMethods: {} as Record<string, () => void>,
+  morphingDuraion: 1,
 };
+particles.geometry.setIndex(null);
 particles.points = new THREE.Points(particles.geometry, particles.material);
+particles.points.frustumCulled = false;
 scene.add(particles.points);
+
+gltfLoader.load("./models.glb", (gltf) => {
+  gltf.scene.children.forEach((child) => {
+    if (child instanceof THREE.Mesh) {
+      particles.modelPositions[child.name] = child.geometry.attributes
+        .position as THREE.BufferAttribute;
+
+      particles.maxPositionCount = Math.max(
+        particles.maxPositionCount,
+        particles.modelPositions[child.name].count
+      );
+    }
+  });
+
+  const sizes = new Float32Array(particles.maxPositionCount);
+  for (let i = 0; i < particles.maxPositionCount; i++) {
+    sizes[i] = Math.random();
+  }
+
+  Object.keys(particles.modelPositions).forEach((key) => {
+    const positions = particles.modelPositions[key];
+    const newArray = new Float32Array(
+      particles.maxPositionCount * positions.itemSize
+    );
+    newArray.set(positions.array);
+    // newArray.fill(9999, positions.array.length);
+    for (let i = positions.count; i < particles.maxPositionCount; i++) {
+      const i3 = i * 3;
+      const randomIndex = Math.floor(Math.random() * positions.count) * 3;
+      newArray[i3] = positions.array[randomIndex];
+      newArray[i3 + 1] = positions.array[randomIndex + 1];
+      newArray[i3 + 2] = positions.array[randomIndex + 2];
+    }
+    particles.modelPositions[key] = new THREE.Float32BufferAttribute(
+      newArray,
+      positions.itemSize
+    );
+  });
+
+  particles.geometry.setAttribute(
+    "position",
+    particles.modelPositions["Suzanne"]
+  );
+  particles.geometry.setAttribute(
+    "aPositionTarget",
+    particles.modelPositions["Suzanne"]
+  );
+  particles.geometry.setAttribute(
+    "aSize",
+    new THREE.Float32BufferAttribute(sizes, 1)
+  );
+
+  Object.keys(particles.modelPositions).forEach((key) => {
+    const methodName = `goTo${key}`;
+    particles.morphingMethods[methodName] = () => {
+      if (
+        particles.modelPositions[key] ===
+        particles.geometry.attributes.aPositionTarget
+      ) {
+        return;
+      }
+      particles.geometry.attributes.position =
+        particles.geometry.attributes.aPositionTarget;
+      particles.geometry.attributes.aPositionTarget =
+        particles.modelPositions[key];
+
+      gsap.fromTo(
+        particles.material.uniforms.uProgress,
+        { value: 0 },
+        { value: 1, duration: particles.morphingDuraion, ease: "linear" }
+      );
+    };
+    gui.add(particles.morphingMethods, methodName);
+  });
+});
 
 const camera = new THREE.PerspectiveCamera(
   35,
@@ -64,6 +152,16 @@ renderer.setClearColor(parameters.clearColor);
 gui.addColor(parameters, "clearColor").onChange(() => {
   renderer.setClearColor(parameters.clearColor);
 });
+gui.addColor(particles.material.uniforms.uColorA, "value");
+gui.addColor(particles.material.uniforms.uColorB, "value");
+gui
+  .add(particles.material.uniforms.uProgress, "value")
+  .min(0)
+  .max(1)
+  .step(0.01)
+  .listen()
+  .name("Progress");
+gui.add(particles, "morphingDuraion").min(0).max(5).step(1).name("Duration");
 
 window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,13 +1,38 @@
+#define DURATION 0.6
+
 uniform vec2 uResolution;
 uniform float uSize;
+uniform float uProgress;
+uniform vec3 uColorA;
+uniform vec3 uColorB;
+
+attribute vec3 aPositionTarget;
+attribute float aSize;
+
+varying vec3 vColor;
+
+#include ./simplex-noise.glsl
 
 void main()
 {
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    float noiseOrigin = simplexNoise3d(position * 0.2);
+    float noiseTartget = simplexNoise3d(aPositionTarget * 0.2);
+    float noise = mix(noiseOrigin, noiseTartget, uProgress);
+    noise = smoothstep(-1.0, 1.0, noise);
+
+    float delay = (1.0 - DURATION) * noise;
+    float end = delay + DURATION;
+    float progress = smoothstep(delay, end, uProgress);
+
+    vec3 mixedPosition = mix(position, aPositionTarget, progress);
+
+    vec4 modelPosition = modelMatrix * vec4(mixedPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
     
-    gl_PointSize = uSize * uResolution.y;
+    gl_PointSize = aSize * uSize * uResolution.y;
     gl_PointSize *= (1.0 / - viewPosition.z);
+
+    vColor = mix(uColorA, uColorB, noise);
 }
